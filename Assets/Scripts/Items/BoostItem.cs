59fab7151750fa27de9265450408088a379f5851
Crokaa <jocroque2@gmail.com>
2025-10-20 23:14:34 +0100

    Some Folder Cleaning. Created a boost item script that will boost one throw of the player.

---
+++ Assets/Scripts/Items/BoostItem.cs
@@ -0,0 +1,33 @@
+using System.Net.NetworkInformation;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoostItem : MonoBehaviour
+{
+    private static readonly float MAXDURATION = 3.0f;
+    private float timer;
+    private bool _isShowing = false;
+    public bool HasShown { get; private set; }
+
+    void Update()
+    {
+        if (_isShowing)
+        {
+            timer += Time.deltaTime;
+            if (timer >= MAXDURATION)
+            {
+                gameObject.SetActive(false);
+                _isShowing = false;
+            }
+        }
+    }
+
+    public void Show()
+    {
+        HasShown = true;
+        gameObject.SetActive(true);
+        _isShowing = true;
+        timer = 0.0f;
+    }
+}
